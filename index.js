af7eb0b29d8e84b12c25f9d87c68e70291445519
Liliia Ustrytska bc2025-3 <liliyaustrytska@gmail.com>
2025-09-30 15:18:06 +0300

    Виконала вимоги частини 2

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-const { program } = require('commander')
+const { program } = require('commander');
+const fs = require('fs');
+const path = require('path');
 
-program.option ('-f, --file <path>', 'шлях до JSON файлу');
+program
+   .option ('-i, --input <path>', 'шлях до файлу для читання (обов`язковий параметр)')
+   .option ('-o, --output <path>', 'шлях до файлу, у якому записується результат')
+   .option ('-d, --display', 'вивести результат у консоль');
 
 program.parse(process.argv);
 const options = program.opts();
-console.log('вибраний файл:', options.file);
+
+if(!options.input){
+    console.error('Please, specify input file');
+    process.exit(1);
+}
+
+if(!fs.existsSync(options.input)){
+    console.error('Cannot find input file');
+    process.exit(1);
+}
+
+let data;
+
+try {
+  const fileContent = fs.readFileSync(options.input, 'utf-8');
+  data = JSON.parse(fileContent);
+
+
+   resultString = JSON.stringify(data, null, 2); 
+} catch (err) {
+  console.error("Error reading or parsing input file");
+  process.exit(1);
+}
+
+if (options.output) {
+  try {
+    fs.writeFileSync(options.output, resultString, 'utf-8');
+  } catch (err) {
+    console.error("Error writing to output file");
+    process.exit(1);
+  }
+}
+
+if (options.display) {
+  console.log(resultString);
+}
+
